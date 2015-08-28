087c22dd5e37f7cb5f8a727d5b27d5a185159115
gatagata <gdgd@users.noreply.github.com>
2015-08-28 06:08:05 +0000

    selenium-server delete

--- src/vanilla.js
+++ src/vanilla.js
@@ -21,11 +21,15 @@ driver.controlFlow().on('uncaughtException', function(err) {
     //検索画面遷移
     driver.get("http://www.vanilla-air.com/jp/");
 
+    driver.findElement(By.css("#reservation_point_select > div:nth-child(1) > div:nth-child(2) > input:nth-child(1)")).click();
+    driver.findElement(By.css("#reservation_point_select > div:nth-child(1) > div:nth-child(2) > ul:nth-child(2) > li:nth-child(1)")).click();
+    driver.findElement(By.css("div.form-item-destination:nth-child(2) > div:nth-child(2) > input:nth-child(1)")).click();
+
     // $("#reservation_point_select input[placeholder='出発地']").attr("data-value","OKA");
     // $("#reservation_point_select input[placeholder='目的地']").attr("data-value","OKA");
 
 　　//xpathがうまく取れない
-    driver.findElement("#reservation_point_select input[placeholder='出発地']").type('data-value','OKA');
+    // driver.findElement("#reservation_point_select input[placeholder='出発地']").type('data-value','OKA');
 
     // driver.findElement(By.xpath("//div[@id='reservation_point_select']/div/div/ul/li")).click();
     // driver.findElement(By.xpath("//div[@id='reservation_point_select']/div[2]/div/ul/li[2]")).click();
