50d96c0544572855d0b94527449b6700c80efd36
Masateru Maruyama <gatagataokuba@gmail.com>
2016-04-21 01:31:21 +0900

    findサンプル修正

--- src/test/dbaccessTest_find.js
+++ src/test/dbaccessTest_find.js
@@ -1,30 +1,28 @@
-//
-//航空マスタからデータを取得するサンプル
-//
+console.log('--start--');
+
+process.on('uncaughtException', function (err) {
+    console.log(err);
+});
 
 var mongo = require('mongoskin');
-var db = mongo.db('mongodb://160.16.95.237:27017/flight', {native_parser:true});
 
-console.log('start');
+var db = mongo.db('mongodb://flight:mC5DJaYi@160.16.95.237/flight', {native_parser:true});
+//バインドすることでオブジェクトとしてflight_route_vanillaをあつかう
+db.bind('flight_route_vanilla');
 
-//bindすることでair_lines_dataコレクションをobjectとしてあつかう
-db.bind('air_lines_data').bind({
-    getByAuthor: function(code, callback) {
-        this.findOne({code: code}, callback);
-    }
+
+db.flight_route_vanilla.find({}).toArray(function(err, items) {
+        db.close();
+        if (err) {
+            console.log('-Erro-');
+            console.log(err);
+            return err;
+        }
+
+        for (var i = 0, size = items.length; i < size; ++i) {
+            console.log('--faind--');
+            console.log(items[i]);
+        }
+        console.log('--end--');
 });
-
-//空港コーが成田のデータを取得
-db.air_lines_data.getByAuthor('NRT', function(err, documents) {
-//        console.log(article);
-
-        //取得するデータ
-        console.log(documents.country);
-        console.log(documents.region.region_name);
-
-});
-
-console.log('close');
-
-db.close();
 
